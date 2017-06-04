41e290a3bfbc08203897e12297aae1d2f56be4aa
Robinson Marquez <robinsonmadrid@outlook.com>
2017-06-04 19:01:42 -0400

    Adding GeoSuggest

--- src/weather/WeatherForm.js
+++ src/weather/WeatherForm.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Geosuggest from 'react-geosuggest';
 
+import './WeatherForm.scss';
 class WeatherForm extends Component {
     state = {  }
     render() {
         return (              
             <form>
-                <input type="text" placeholder="Enter city name"/>
+                {/*<input type="text" placeholder="Enter city name"/>*/}
+                <Geosuggest placeholder="Enter city name"/>
                 <button type="submit" className="button expanded">Get Weather</button>
             </form>
         );
